1bb7e151b87bda4b0d14e9456f65382a134cd655
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-03 05:43:15 +0000

    update

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,7 +49,7 @@ class Server:
                 "page": page,
                 "data": self.get_page(page, page_size),
                 "next_page": page + 1 if page + 1 <= total_pages else None,
-                "prev_page": page - 1 if page - 1 >= 1 else None,
+                "prev_page": page - 1 if page - 1 > 1 else None,
                 "total_pages": total_pages
         }
 
