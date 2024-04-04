80ca5f85dc2d5e80fdbfd2d1c6767a10dd9b86b9
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-04 01:19:05 +0000

    test lru

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+LRU caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """init"""
+        super().__init__()
+        self.cache_order = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_order.remove(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.cache_order.pop(0)
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+        self.cache_order.append(key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        
+        self.cache_order.remove(key)
+        self.cache_order.append(key)
+
+        return self.cache_data[key]
+
