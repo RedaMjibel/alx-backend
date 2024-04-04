9f5b9cbeccaee1c10b68790bcce01c96c08a5941
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-04 00:28:27 +0000

    pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -24,7 +24,7 @@ class FIFOCache(BaseCaching):
 
         self.cache_data[key] = item
         self.cache_order.append(key)
-    
+
     def get(self, key):
         """ return the value in self.cache_data linked to key """
         if key is None or key not in self.cache_data:
