d8147b8fa1beaaab68f588010da6b63e7b6ee1e5
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-10 04:37:21 +0000

    simple flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+0. Basic Flask app
+"""
+
+from flask import Flask, request
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('0-index.html', strict_slashes=False)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
