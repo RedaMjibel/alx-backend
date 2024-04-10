08487d749d9ec2f366a56c329d83a903c8db2b4d
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-10 05:03:53 +0000

    insantiate babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
+
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template
+
+babel = Babel()
+
+
+class Config:
+    """ includes languages """
+    LANGUAGES = ["en", "fr"]
+
+babel.default_locale = "en"
+babel.default_timezone = "UTC"
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """ hello world """
+    return render_template('0-index.html', strict_slashes=False)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
