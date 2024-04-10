68c9fc792e7b283e5935aac1a45114ceadc761de
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-10 04:38:44 +0000

    pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,9 +10,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template('0-index.html', strict_slashes=False)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
